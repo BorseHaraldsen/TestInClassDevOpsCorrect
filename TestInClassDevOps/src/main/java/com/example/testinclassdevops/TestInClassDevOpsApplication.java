76555b8ffdf4d18362655d4b059b9d511f1384a6
August Borse Haraldsen <144022756+BorseHaraldsen@users.noreply.github.com>
2024-10-21 23:04:46 +0200

    Initial commit

---
+++ TestInClassDevOps/src/main/java/com/example/testinclassdevops/TestInClassDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testinclassdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestInClassDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestInClassDevOpsApplication.class, args);
+	}
+
+}
---
+++ TestInClassDevOps/src/test/java/com/example/testinclassdevops/TestInClassDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testinclassdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestInClassDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
